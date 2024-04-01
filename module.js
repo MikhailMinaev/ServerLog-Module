344713466c0c79ba1d5eac80c62d486a544dec0b
MikhailMinaev <inbox@mikhailminaev.com>
2024-04-01 13:25:38 +0300

    Init system information log structure

--- module.js
+++ module.js
@@ -1,3 +1,62 @@
+const systeminformation = require('systeminformation')
+
+// Helpers
+
+systemInformationServices = {
+    time:               '  Time   ',
+    system:             ' System  ',
+    bios:               '  Bios   ',
+    baseboard:          'Baseboard',
+    cpu:                'CPU      ',
+    memory:             'Memory   ',
+    graphics:           'Graphics ',
+    osInfo:             'OS       ',
+    versions:           'Software ',
+    diskLayout:         'Disks    ',
+    networkInterfaces:  'Network  ',
+}
+
+function unixTimeConverter(UNIX_timestamp){
+
+    const timeStamp = new Date(UNIX_timestamp)
+
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+
+    //UTC Time
+    const UTCYear = timeStamp.getUTCFullYear();
+    const UTCMonth = timeStamp.getUTCMonth();
+    const UTCMonthString = months[timeStamp.getUTCMonth()];
+    const UTCDay = timeStamp.getUTCDate();
+    const UTCDayString = days[timeStamp.getUTCDay()];
+    const UTCHour = timeStamp.getUTCHours();
+    const UTCMinutes = timeStamp.getUTCMinutes();
+    const UTCSeconds = timeStamp.getUTCSeconds();
+    
+    //local Time
+    const localYear = timeStamp.getFullYear();
+    const localMonth = timeStamp.getMonth();
+    const localMonthString = months[timeStamp.getMonth()];
+    const localDay = timeStamp.getDate();
+    const localDayString = days[timeStamp.getDay()];
+    const localHour = timeStamp.getHours();
+    const localMinutes = timeStamp.getMinutes();
+    const localSeconds = timeStamp.getSeconds();
+
+    const UTCTime =             `${UTCYear}/${String('0' + UTCMonth).substr(-2)}/${String('0' + UTCDay).substr(-2)} ${String('0' + UTCHour).substr(-2)}:${String('0' + UTCMinutes).substr(-2)}:${String('0' + UTCSeconds).substr(-2)} ${timeStamp.toUTCString().substring(26)}`;
+    const UTCFormatedTime =     `${UTCDayString}, ${UTCYear} ${UTCMonthString} ${String('0' + UTCDay).substr(-2)} ${String('0' + UTCHour).substr(-2)}:${String('0' + UTCMinutes).substr(-2)}:${String('0' + UTCSeconds).substr(-2)} ${timeStamp.toUTCString().substring(26)}`;
+    const localTime =           `${localYear}/${String('0' + localMonth).substr(-2)}/${String('0' + localDay).substr(-2)} ${String('0' + localHour).substr(-2)}:${String('0' + localMinutes).substr(-2)}:${String('0' + localSeconds).substr(-2)} ${timeStamp.toString().substring(25)}`;
+    const localFormatedTime =   `${localDayString}, ${localYear} ${localMonthString} ${String('0' + localDay).substr(-2)} ${String('0' + localHour).substr(-2)}:${String('0' + localMinutes).substr(-2)}:${String('0' + localSeconds).substr(-2)} ${timeStamp.toString().substring(25)}`;
+
+    return { 
+        UTCTime: UTCTime, 
+        UTCFormatedTime: UTCFormatedTime, 
+        localTime: localTime,
+        localFormatedTime: localFormatedTime
+    };
+
+}
+
 //Functions
 
 let appName;
@@ -112,6 +171,10 @@ const serviceSuccess = (serviceName, text) => {
     console.log(serverName('green') + terminalText(" " + serviceName + " ", 'white', 'green', false) + divider('green', '') + terminalText(text, 'white', '', false))
 }
 
+const message = (infoText) => {
+    console.log(serverName('') + dividerBack('blue', '') + terminalText("   Info  ", 'white', 'blue', false) + divider('purple', '') + terminalText(infoText, 'blue', '', false))
+}
+
 const error = (errorText) => {
     console.log(serverName('') + dividerBack('red', '') + terminalText("  Error ", 'white', 'red', false) + divider('red', '') + terminalText(errorText, 'red', '', false))
 }
@@ -128,20 +191,62 @@ const success = (infoText) => {
     console.log(serverName('') + dividerBack('green', '') + terminalText("   Info  ", 'white', 'green', false) + divider('green', '') + terminalText(infoText, 'white', '', false))
 }
 
+const systemTimeInformation = async () => {
+
+    const time = await systeminformation.time()
+
+    const formatedTime = unixTimeConverter(time.current)
+
+    serviceInfo(systemInformationServices.time,    `\x1b[1mUTC Time:      \x1b[0m${formatedTime.UTCFormatedTime}`)
+    serviceInfo(systemInformationServices.time,    `\x1b[1mLocal Time:    \x1b[0m${formatedTime.localFormatedTime}`)
+    serviceInfo(systemInformationServices.time,    `\x1b[1mTimezone:      \x1b[0m${time.timezone}`)
+    serviceInfo(systemInformationServices.time,    `\x1b[1mTimezone:      \x1b[0m${time.timezoneName}`)
+
+}
+
+const systemDataInformation = async () => {
+
+    const system = await systeminformation.system()
+
+    serviceInfo(systemInformationServices.system,  `\x1b[1mUUID:          \x1b[0m${system.uuid}`)
+
+}
+
+const systemBiosInformation = async () => {
+
+    const bios = await systeminformation.bios()
+
+    serviceInfo(systemInformationServices.bios,    `\x1b[1mBIOS Vendor :  \x1b[0m${bios.vendor}`)
+    serviceInfo(systemInformationServices.bios,    `\x1b[1mBIOS Version:  \x1b[0m${bios.version}`)
+
+}
+
+const systemInformation = async () => {
+    systemTimeInformation()
+    systemDataInformation()
+    systemBiosInformation()
+}
+
 // Module init
 
 module.exports = {
 
+    appName: setAppName,
+
     log,
     heading,
+    serviceMessage,
     serviceInfo,
-    serviceError,
     serviceWarning,
+    serviceError,
     serviceSuccess,
-    error,
-    warning,
+    message,
     info,
+    warning,
+    error,
     success,
-    appName: setAppName
+    
+    systemInformation,
+    systemTimeInformation
 
 }
